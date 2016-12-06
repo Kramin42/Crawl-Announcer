f8183e6812fc87e506ad91caae06cc9ce8da6032
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-12-06 19:31:55 +1300

    add johnstein and chequers to admin list

--- constants.js
+++ constants.js
@@ -8,4 +8,4 @@ exports.delay_avg_p= 0.01;
 exports.difficulty_map = {"1": "standard", "2": "challenge", "3": "nightmare"};
 exports.pubsub_host = "api.crawl.project357.org";
 exports.catchup_limit = 10;
-exports.admins = ['Kramin','kramin','Kramin42'];
+exports.admins = ['Kramin','kramin','Kramin42','johnstein','chequers'];
