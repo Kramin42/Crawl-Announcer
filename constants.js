e5f2891aed4857344c306099e3671251e33d7254
Cameron Dykstra <dykstra.cameron@gmail.com>
2017-03-07 13:29:32 +1300

    add hardcoded admin

--- constants.js
+++ constants.js
@@ -9,4 +9,4 @@ exports.difficulty_map = {"1": "standard", "2": "challenge", "3": "nightmare"};
 exports.pubsub_host = "api.crawl.project357.org";
 exports.pubsub_protocol = "https://";
 exports.catchup_limit = 50;
-exports.admins = ['Kramin','kramin','Kramin42','johnstein','chequers'];
+exports.admins = ['Kramin','kramin','Kramin42','johnstein','chequers','alexjurkiewicz'];
